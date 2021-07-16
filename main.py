f49af63e7610b7ae07a9f16a37de5008148c84fd
codemkim <inkin61@naver.com>
2021-07-16 17:40:04 +0900

    Slack_message_code(7/16)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 
-
+# 파이썬에서 슬랙으로 메시지 보내는 함수
 def post_message(token, channel, text):
     response = requests.post("https://slack.com/api/chat.postMessage",
                              headers={"Authorization": "Bearer " + token},
@@ -8,12 +8,19 @@ def post_message(token, channel, text):
                              )
     print(response)
 
+# 시크릿 키 가져오기
+with open('.env') as f:
+    read = f.readline()
 
-myToken = "xoxp-2266850767047-2305263783744-2294418003393-7fdef098d75a0bec073d5a907b6d0bd4"
+# 슬랙 api 토큰 주소
+myToken = read
 
-text = '주환이 ㅎㅇ'
-
+# 슬랙 채널명
 channel_name = '#stock'
 
+# 보낼 메시지
+text = 'muuuuuyaaaho'
+
+# 메시지 전송 함수 호출
 post_message(myToken, channel_name, text)
 
