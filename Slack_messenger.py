1bd9d15d582760ad97c07b8a43303f480b4a50e4
codemkim <inkin61@naver.com>
2021-07-17 16:27:25 +0900

    finance_samsung_price(7/17)

--- Slack_messenger.py
+++ Slack_messenger.py
@@ -1,31 +1,31 @@
 import requests
 from bs4 import BeautifulSoup as bs
-#
-# # 파이썬에서 슬랙으로 메시지 보내는 함수
-# def post_message(token, channel, text):
-#     response = requests.post("https://slack.com/api/chat.postMessage",
-#                              headers={"Authorization": "Bearer " + token},
-#                              data={"channel": channel, "text": text}
-#                              )
-#     print(response)
-#
-# # 시크릿 키 가져오기
-# with open('.env') as f:
-#     read = f.readline()
-#
-# # 슬랙 api 토큰 주소
-# myToken = read
-#
-# # 슬랙 채널명
-# channel_name = '#stock'
-#
-# # 보낼 메시지
-# text = 'muuuuuyaaahooooo!!!'
-#
-# # 메시지 전송 함수 호출
-# post_message(myToken, channel_name, text)
+import time
+
+def set_message():
+    # 시크릿 키 가져오기
+    with open('.env') as f:
+        read = f.readline()
+
+    # 슬랙 api 토큰 주소
+    myToken = read
+
+    # 슬랙 채널명
+    channel_name = '#stock'
+
+    return myToken, channel_name
+
+# 파이썬에서 슬랙으로 메시지 보내는 함수
+def post_message(text):
+    token, channel = set_message()
+    response = requests.post("https://slack.com/api/chat.postMessage",
+                             headers={"Authorization": "Bearer " + token},
+                             data={"channel": channel, "text": text}
+                             )
+    print(response)
 
 
+# 주소 받아오기
 def get_soup(company_code):
     # 네이버 금융 url
     url = 'https://finance.naver.com/item/main.nhn?code='+company_code
@@ -38,6 +38,7 @@ def get_soup(company_code):
 
     return soup
 
+# 가격정보 받아오기
 def get_price(company_code):
     soup = get_soup(company_code)
 
@@ -45,10 +46,24 @@ def get_price(company_code):
 
     blind_s = today_s.find('span', {'class':'blind'})
 
-    return blind_s.text
+    return blind_s.text.replace(',', '')
 
 
 
-s_price = get_price('000660')
 
-print(s_price)
+
+while True:
+    s_price = int(get_price('005930'))
+
+    if s_price > 80000:
+        # 메시지 전송 함수 호출
+        post_message(s_price)
+
+    elif s_price <= 79000:
+        # 메시지 전송 함수 호출
+        post_message(s_price)
+
+    elif s_price == 79800:
+        post_message(s_price)
+
+    time.sleep(5)
--- finance_chart.py
+++ finance_chart.py
@@ -1,11 +1,26 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-
 url = 'https://finance.naver.com/sise/sise_rise.nhn'
+
 
 res = requests.get(url)
 
-soup = bs(res, 'html.parser')
 
-print(soup)
+soup = bs(res.content, 'html.parser')
+
+title = soup.find('tr', {'class':'title'})
+
+print(title)
+#
+#
+# stock_code =[]
+#
+# i = 1
+#
+# for i in range(len(temp)):
+#     temp2 = temp[i][44:50]
+#     stock_code.append(temp2)
+#     i = i+1
+#
+# print(stock_code)
