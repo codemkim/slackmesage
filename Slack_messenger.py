e3a544476142413038489d19d1e7b661cb5ccef1
codemkim <inkin61@naver.com>
2021-07-19 10:42:09 +0900

    auto credit (7/19)

--- Slack_messenger.py
+++ Slack_messenger.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import time
+from datetime import datetime
+
 
 def set_message():
     # 시크릿 키 가져오기
@@ -48,28 +50,45 @@ def get_price(company_code):
 
     return blind_s.text.replace(',', '')
 
-#시초가
-first_price = int(get_price('005930'))
 
 # 상승 / 하락 %
-max_p = 0.01
-min_p = 0.01
+max_p = 0.001
+min_p = 0.001
+
+# 나의 평단
+first_price = 79800
+
 # 목표 가격
 target_max_price = first_price + (first_price * max_p)
 target_min_price = first_price - (first_price * min_p)
 
+
 # 자동 알림 코드
+
 while True:
     s_price = int(get_price('005930'))
 
+
     if s_price >= target_max_price:
-        post_message(s_price)
+        now = str(datetime.now())
+        post_message(f'삼성전자 : {str(s_price)} / 시간 : {now[:-6]}')
 
         target_max_price = s_price + (first_price * max_p)
 
+        target_min_price = s_price - (first_price * min_p)
+
+        print(target_max_price, target_min_price)
+
     elif s_price <= target_min_price:
-        post_message(s_price)
+        now = str(datetime.now())
+        post_message(f'삼성전자 : {str(s_price)} / 시간 : {now[:-6]}')
 
-        target_min_price = s_price + (first_price * min_p)
+        target_max_price = s_price + (first_price * max_p)
 
-    time.sleep(5)
+        target_min_price = s_price - (first_price * min_p)
+
+        print(target_max_price, target_min_price)
+
+    time.sleep(20)
+
+
